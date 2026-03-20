ff1d27cf283e596b1223d79c1a0e68b8813bd83f
JDOT <JDON219206@gmail.com>
2026-03-20 21:58:20 +1100

    Add test input file for sorting algorithm

--- src/Bubble_sort.py
+++ src/Bubble_sort.py
@@ -13,15 +13,36 @@ class bubble_sort:
                     sample[j + 1] = sample[j]
                     sample[j] = temp
 
-                    
-
+    def print_original(self, number):
+        print("\n==================================================")
+        print("Original")
+        print("==================================================\n")
+        for i in range(len(number)):
+          if i + 1 == len(number):
+               print(f"{number[i]}]")
+          elif i == 0:
+               print(f"[{number[i]}, ",end="")
+          else:
+               print(number[i],end=", ")  
 
 
     def print(self, sample, time):
-        print(f"Time taken = {time:.6f} seconds")
+        print("\n==================================================")
+        print("Sorted Output")
+        print("==================================================\n")
         for i in range(len(sample)):
-            print(sample[i],end= " ")
-
+            if i == 0:
+                print(f"[{sample[i]}, ",end="")
+            elif i + 1 == len(sample):
+                print(f"{sample[i]}]",end="\n")
+            else:
+                print(sample[i],end= ", ")
+        print("\n==================================================")
+        print("Bubble Sort Result:")
+        print(f"Size of array = {len(sample)}")
+        print(f"Time taken = {time:.6f} seconds")
+        print
+        print("==================================================\n")
                         
 
 
--- src/Main.py
+++ src/Main.py
@@ -2,31 +2,45 @@ from Bubble_sort import bubble_sort
 import time
 import os
 def main():
+
     while True:
+        print("==================================================")
+        print("Sorting")
+        print("==================================================")
         print("[1] Bubble Sort with File")
         print("[-1] Exit")
+        print("==================================================")
 
-        user_input = input("Select Option")
-        if user_input == 1:
-             file_name = input("Enter the file name, example test1.in")
-             bubblesort(file_name)
-        elif user_input == -1:
+        user_input = input("Select Option: ")
+        if user_input == "1":
+             file = get_file()
+             print(file)
+             bubblesort(file)
+        elif user_input == "-1":
              exit()
+             
+def get_file():
+     file = input("Enter File Name: ")
+     return os.path.join("tests", file)
 
 def bubblesort(file):
+        algorithm = bubble_sort()
         try:
              with open(file) as f:
                   numbers_str = f.readlines()
         except FileNotFoundError:
              print("File does not exist")
              return
+        numbers = [int(number) for number in numbers_str]
+        algorithm.print_original(numbers)
+
         time_taken = 0
-        algorithm = bubble_sort()
-        sample = [0, -10, 5, 3, 3, -1, 999, -1000, 50, 2, 2, 1, -10, 999, 7]
+
         start_time = time.time()
-        algorithm.sort(sample)
+        algorithm.sort(numbers)
         end_time = time.time()
+    
         time_taken = end_time - start_time
-        algorithm.print(sample, time_taken)
+        algorithm.print(numbers, time_taken)
 if __name__ == "__main__":
     main()
